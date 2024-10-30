44800462ab5a4777987a24f977bbe06903c8b881
AkashMAbrrar <madborakash48@gmail.com>
2024-10-30 09:00:34 +0600

    avoid reload by using Link tag for navbar

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
 const Header = () => {
   return (
     <div>
       <h2>This is header and navbar</h2>
       <nav>
-        <li></li>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact Us</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
       </nav>
     </div>
   );
